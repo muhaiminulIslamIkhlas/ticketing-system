506adb4502611d8b7a82fafb824379457abf726b
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2023-11-07 00:08:22 +0600

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+    if (!process.env.JWT_KEY) throw new Error('JWT secret not found');
+
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('connected to mongodb');
+    } catch (error) {
+        console.log(error);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!!!!');
+    })
+}
+
+start();
+
